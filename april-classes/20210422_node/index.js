9d34c749864f598e78263d15b50819319885779c
Amopho <amopho78@gmail.com>
2021-04-22 14:04:45 +0200

    js review

---
+++ april-classes/20210422_node/index.js
@@ -0,0 +1,20 @@
+// let classArr = ["classOne", "classTwo"];
+
+// moreClass.classList.add(...classArr);
+// console.log(moreClass.classList);
+
+// //
+// moreClass.style.fontSize = "2.3rem";
+
+// moreClass.style.color = "red";
+
+const words = ["hi", "a", "web", "development"];
+const sortedArr = words.sort((a, b) => b.length - a.length);
+
+// true && true = true
+console.log(true && true);
+// true && false = false
+console.log(true && true);
+// true || true = true
+// false || true = true
+// false || false = false
