cd65d1fadf7fe19601cfe6eae1b4a40aa88038bc
Amopho <amopho78@gmail.com>
2021-05-05 11:00:54 +0200

    todays classes

---
+++ april-classes/20210505_JSON/scripts/data.js
@@ -0,0 +1,23 @@
+// const obj = [
+//   {
+//     name: "Hadi",
+//   },
+// ];
+
+// //step1 making obj into JSON
+// let data = JSON.stringify(obj);
+
+// // 2 ways
+
+// // first - step 2 export
+// export default data;
+
+const obj = {
+  name: "HAdi",
+  age: 31,
+};
+const obj2 = {};
+//i want you to be a json Object
+let data = JSON.stringify(obj);
+//i want you to be out
+export default data;
---
+++ april-classes/20210505_JSON/scripts/index.js
@@ -0,0 +1,13 @@
+// import data from "./data.js";
+// let parsedData = JSON.parse(data);
+// console.log(parsedData);
+
+// let parsedData = JSON.parse(data);
+
+//i want to change the name, not keeping the same name, you need the default going on
+
+import { data } from "./data.js";
+let parsedData = JSON.parse(data);
+const section = document.querySelector("#bread");
+console.log(parsedData);
+parsedData;
