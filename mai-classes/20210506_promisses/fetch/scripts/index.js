351e692ea22b07c3964887d17a83f273c2d4083a
Amopho <amopho78@gmail.com>
2021-05-11 22:58:29 +0200

    api todos

--- mai-classes/20210506_promisses/fetch/scripts/index.js
+++ mai-classes/20210506_promisses/fetch/scripts/index.js
@@ -15,6 +15,45 @@ function fetchJsonFile() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      let user;
-    });
+      let userCard = "<h2>All Users</h2>";
+      data.forEach((user) => {
+        let { id, name, city, age, photo } = user;
+        userCard += `<div class='card' id=${id}>
+        <div class='avatar'>${photo}</div>
+        ${name}, <span>${age}</span> old, and I live in <span>${city}</span>
+        </div>`;
+      });
+      document.querySelector(".result").innerHTML = userCard;
+    })
+    .catch((err) => console.log(`So, this is what happened ${err}`));
+}
+
+// API
+//  Application Programming Interface
+
+// https://jsonplaceholder.typicode.com/
+
+// we will use
+// https://jsonplaceholder.typicode.com/photos
+//fetch by "get"
+// a link to fake data
+//go fot them and then- anonymous function, with response as an argument, which uses method .json on this responsed data
+// another function which processes (stringified into JS object) data and
+function fetchApi() {
+  fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let userCard = "<h2>Images API response</h2>";
+      data.forEach((img) => {
+        let { albumId, id, title, url, thumbnailUrl } = img;
+        userCard += `<div id=${id}>
+      <img src=${thumbnailUrl}>
+      <h3>${title}</h3>
+      <img src=${url} alt=${title}> 
+      </div>`;
+      });
+      document.querySelector(".result").innerHTML = userCard;
+    })
+    .catch((err) => console.log(`So this is what happened  ${err}`));
 }
