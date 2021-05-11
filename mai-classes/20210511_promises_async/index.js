bec6720f1e50697679642dc3453bd2ca49573480
Amopho <amopho78@gmail.com>
2021-05-11 11:34:40 +0200

    todays session

--- mai-classes/20210511_promises_async/index.js
+++ mai-classes/20210511_promises_async/index.js
@@ -19,3 +19,46 @@ const userStatus = new Promise((resolve, reject) => {
     });
   }
 });
+
+userStatus
+  .then((obj) => {
+    document.querySelector(
+      ".result"
+    ).innerHTML = `${obj.status} and he/she is ${obj.facie}`;
+  })
+  .catch((err) => {
+    document.querySelector(
+      ".result"
+    ).innerHTML = `${err.status} and he/she is ${err.emoji}`;
+  });
+
+function stepOne(userName) {
+  return new Promise((resolve, reject) => {
+    console.log("User data being loaded");
+    if (userName == "Zain") {
+      resolve("Welcome back");
+    } else {
+      reject("Sorry IDK you");
+    }
+  });
+}
+
+function secondStep(response) {
+  return new Promise((resolve) => {
+    console.log("Still loading");
+    resolve(`We ve been waiting for you ${response}`);
+  });
+}
+
+async function startSomething() {
+  try {
+    const response = await stepOne("Nancy");
+    console.log("response just arrived");
+    const secondResponse = await secondStep(response);
+    console.log(secondResponse);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+startSomething();
