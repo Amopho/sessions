931aeab9abcbe67ac60d0902d78781925af3f1f3
Amopho <amopho78@gmail.com>
2021-05-26 15:31:01 +0200

    practice on state

--- mai-classes/20210526_react_sassy/to-do-app/to-do-app/src/App.js
+++ mai-classes/20210526_react_sassy/to-do-app/to-do-app/src/App.js
@@ -1,19 +1,27 @@
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [list, setList] = useState([]);
+
+  function changeHandle(e) {
+    console.log(e.target.value);
+    setUserInput(e.target.value.trim());
+  }
+  function add(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={add}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={changeHandle}
+          placeholder="type something"
+        ></input>
+        <input type="submit" value="Add"></input>
+      </form>
     </div>
   );
 }
