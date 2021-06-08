340fd54db5fe8273aa66a1c0e82084a60b3f4e65
Amopho <amopho78@gmail.com>
2021-06-08 14:21:09 +0200

    axios

--- june-classes/20210608_axios/src/App.js
+++ june-classes/20210608_axios/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Loading from "./components/Loading";
+import Country from "./components/Country";
 import "./App.css";
 
 const App = () => {
@@ -18,17 +20,33 @@ const App = () => {
   }
   function submitHandle(e) {
     e.preventDefault();
+    let textToURL = encodeURIComponent(userInput);
+    console.log(textToURL);
+    let endPoint = `https://restcountries.eu/rest/v2/name/${textToURL}`;
+
+    // fetch(endPoint)
+    //   .then((res) => res.json())
+    //   .then((data) => setResults(data));
+
+    axios(endPoint)
+      .then(({ data }) => setResults(data))
+      .catch((err) => console.log(`You have an ${err}`));
   }
-
-  let endpoint = "";
-
-  axios(endPoint).then((res) => console.log(res));
-
-  // fetch(endPoint)
-  //   .then((res) => res.json())
-  //   .then((data) => setResults(data));
-
-  return <React.Fragment></React.Fragment>;
+  if (loading) return <Loading />;
+  return (
+    <React.Fragment>
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={changeHandle}
+          placeholder="Write a country name"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <Country results={results} />
+    </React.Fragment>
+  );
 };
 
 export default App;
--- june-classes/20210608_axios/src/components/Loading.js
+++ june-classes/20210608_axios/src/components/Loading.js
@@ -1,3 +1,12 @@
 import React from "react";
 
-const Loading 
+const Loading = () => {
+  return (
+    <React.Fragment>
+      <h4>:woman_cartwheeling:</h4>
+      <h6>Loading...</h6>
+    </React.Fragment>
+  );
+};
+
+export default Loading;
