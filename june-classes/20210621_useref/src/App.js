0a31b6dc7db5dff0013874a7e63d715d201a7be6
Amopho <amopho78@gmail.com>
2021-06-23 12:59:13 +0200

    useref practice

---
+++ june-classes/20210621_useref/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* useRef-  short from reference, I grab a variable and may reference it in other objectt is not similar with useState but they share some common things. It may be a replacement for ID in DOM. WHenever you will use an useRef it will come back with an object. A lot of properties- current- on which projwct you are currently in*/}
+    </div>
+  );
+}
+
+export default App;
