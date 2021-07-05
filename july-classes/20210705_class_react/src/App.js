4556b656b38473427b2c44254754f5b1cb529254
Amopho <amopho78@gmail.com>
2021-07-05 14:20:25 +0200

    functions in class component

--- july-classes/20210705_class_react/src/App.js
+++ july-classes/20210705_class_react/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { add, display } from "./helpers";
 
 // React Hooks
 // function App() {
@@ -15,15 +16,35 @@ export default class App extends React.Component {
       isLogged: false,
       data: { name: "Licia", age: 34 },
     };
+    this.sum = function () {
+      console.log("I am a function built into constructor");
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  // ES6 - doesnt need to be binded
+  print = () =>
+    console.log(
+      "Hi I am ES6 style function, I dont have to be binded inside the constructor"
+    );
+  handleSubmit() {
+    console.log("Please, bind me so I could work");
   }
   render() {
     console.log("I work inside any method of class component");
+    this.newUser = () => console.log("i am inside render");
+
+    this.newUser();
+
     return (
       <div>
         <h1>
           Hi, I am coming from class component {this.state.data.name}, I am{" "}
           {this.state.data.age}
         </h1>
+        {this.print()}
+        {this.handleSubmit()}
+        {display("Hi, React")}
+        {this.sum()}
       </div>
     );
   }
